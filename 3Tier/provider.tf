4f68d1d7eeae6c52aba2058f77d4c66ee5e6d3d5
Jeevanreddy512 <91076918+Jeevanreddy512@users.noreply.github.com>
2024-06-24 12:48:37 +0530

    Create provider.tf

---
+++ 3Tier/provider.tf
@@ -0,0 +1,6 @@
+#SOURCE == https://medium.com/@midejoseph24/create-a-highly-available-3-tier-architecture-using-terraform-fd426f152708
+# WRITER = Joseph Ariyo [medium.com]
+provider "aws" {
+  region = "us-east-1"
+  #profile = "vs-code"
+}
